1dd163c35888bd036e2f80ba6a943988f065c5fa
Viktorija Filipovic <vifi@zuehlke.com>
2015-10-15 14:55:00 +0200

    Added starter project for workshop

---
+++ OpenITWorkshop - Starter/js/game - starter.js
@@ -0,0 +1,13 @@
+var choices = ['rock', 'paper', 'scissors'];
+	
+// function for start game
+
+// function for start round
+
+// function to handle player's choice
+
+// function to generate RANDOM computer's choice
+
+// function for calculating the round result
+
+// function to calculate score
